e09209dff0b8b6efc8e88a047878debec3845bac
Mykhailo Maryn <Mykhailo Maryn>
2017-05-11 08:53:15 +0300

    File Manager

---
+++ script/app.js
@@ -0,0 +1,209 @@
+
+var App = (function () {
+
+    let Main = {};
+
+    let store = window.localStorage;
+
+    Main = {
+		/*-----
+			Function: Initializes the Mainlication
+		-----*/
+        init: function () {
+            let input = document.getElementById("fileURL");
+            input.addEventListener("change", Main.setDate);
+
+            Main.getAllFiles();
+        },
+
+        data: JSON.parse(store.getItem("newFiles")) || [],
+
+        createfile: () => {
+            let obj = {
+                name: "File",
+                size: "6000",
+                type: "txt",
+                data: "today",
+            };
+            let newObj = Main.data.push(obj);
+            store.setItem("newFiles", JSON.stringify(newObj));
+        },
+
+        setDate: (e) => {
+            let files = e.target.files;
+
+            Main.data = [];
+            store.clear();
+
+            for (let i = 0; i < files.length; i++) {
+                let date = new Date();
+
+                Main.data.push({
+                    id: i,
+                    name: files[i].name,
+                    size: files[i].size,
+                    type: files[i].type || "none",
+                });
+            };
+
+            let arrToStore = JSON.stringify(Main.data);
+            store.setItem("newFiles", arrToStore);
+            Main.getAllFiles();
+        },
+
+        getAllFiles: function () {
+            let tbody = document.getElementById('output'); // gets the container element
+            tbody.innerHTML = ''; // resets the list to nothing
+
+            Main.data.map(files => {
+                let tr = document.createElement('tr');
+                tr.setAttribute("id", "" + files.id + "");
+                let parseName = files.name;
+                let parseSize = Main.utility.parseSize(files.size);
+
+                let fileName = document.createElement('td'); // NAme element
+                let fileSize = document.createElement('td'); // NAme element
+                let fileType = document.createElement('td'); // NAme element
+
+                fileName.appendChild(document.createTextNode(parseName));
+                fileSize.appendChild(document.createTextNode(parseSize));
+                fileType.appendChild(document.createTextNode(files.type));
+
+                tr.className = 'file'; // add class
+                tr.appendChild(fileName);
+                tr.appendChild(fileSize);
+                tr.appendChild(fileType);
+
+                tbody.appendChild(tr);
+            })
+        },
+
+
+        getElement: (e) => {
+            console.log(e.target);
+        },
+
+        /*-----
+            Function: Deletes a file
+        -----*/
+        deleteFile: function (el) {
+            el.className.add("");
+            let i = 0;
+            Main.data.find(element => {
+                if (element.id == el) {
+                    i = Main.data.indexOf(element);
+                }
+            });
+
+            Main.utility.returnError(`File ${Main.data[i].name} was deleted.`);
+
+            Main.data.splice(i, 1); // remove file from data array
+            store.setItem("newFiles", JSON.stringify(Main.data));
+            Main.getAllFiles();
+        },
+
+        addFile: () => {
+            let now = new Date;
+            let hour = now.getTime();
+            let res = prompt("Plz write file name like example.txt!");
+
+            let name = res.toString();
+            let notFound = res.indexOf(".");
+
+
+            if (name.length <= 0 || notFound == -1) {
+                Main.utility.returnError("Please enter Correct Name")
+
+            }
+
+            else {
+
+                let name = res.slice(0, res.indexOf("."));
+                let fileExecution = res.slice(res.indexOf(".") + 1);
+
+                Main.data.unshift({
+                    id: hour,
+                    name: name,
+                    size: 125,
+                    type: fileExecution
+                });
+
+                Main.utility.returnError(`File ${res} was added.`);
+            }
+
+            store.setItem("newFiles", JSON.stringify(Main.data));
+            Main.getAllFiles();
+        },
+
+        renameFile: (el) => {
+            let i = 0;
+            let newName;
+            Main.data.find(element => {
+                if (element.id == el) {
+                    i = Main.data.indexOf(element);
+                    newName = prompt("Enter the new file name!");
+                    if (newName.length > 0) {
+                        Main.data[i].name = newName;
+                    }
+
+                    Main.utility.returnError("File was renamed.");
+                }
+            });
+
+            store.setItem("newFiles", JSON.stringify(Main.data));
+            Main.getAllFiles();
+            console.log(el);
+        },
+
+        utility: {
+            /*-----
+                Function: Grabs element based on attribute from the entire page
+            -----*/
+            getElementByAttribute: attribute => {
+                let everything = document.getElementsByTagName('*');
+                let matchingElements = [];
+
+                for (let i = 0; i < everything.length; i++) {
+                    if (everything[i].getAttribute(attribute)) {
+                        matchingElements.push(everything[i]);
+                    }
+                }
+                return matchingElements;
+            },
+
+            /*-----
+            Function: Parse file size
+            -----*/
+            parseSize: bytes => {
+                let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+                if (bytes == 0) return '0 Byte';
+                let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+                return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+            },
+
+            returnError: text => {
+
+                let tip = document.getElementById("tip");
+                if (text) {
+                    tip.className = "show";
+                    tip.innerHTML = text;
+
+                    setTimeout(function () { tip.className = tip.className.replace("show", ""); }, 3000);
+
+                }
+                else {
+                    console.log("ass");
+                }
+            }
+        }
+    }
+
+    Main.init(); // initialize the Main
+
+    return {
+        deleteFile: Main.deleteFile,
+        addFile: Main.addFile,
+        renameFile: Main.renameFile,
+    };
+})();
+
---
+++ script/menu.js
@@ -0,0 +1,213 @@
+let Menu = (function (App) {
+
+    "use strict";
+
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //
+    // H E L P E R    F U N C T I O N S
+    //
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+
+    function clickInsideElement(e, className) {
+        let el = e.srcElement || e.target.parentNode;
+
+        if (el.classList.contains(className)) {
+            return el;
+        } else {
+            while (el = el.parentNode) {
+                if (el.classList && el.classList.contains(className)) {
+                    return el;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    function getPosition(e) {
+        let posx = 0;
+        let posy = 0;
+
+        if (!e) var e = window.event;
+
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        } else if (e.clientX || e.clientY) {
+
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+        }
+
+        return {
+            x: posx,
+            y: posy
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //
+    // C O R E    F U N C T I O N S
+    //
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * letiables.
+     */
+
+    let menuActions = {};
+
+    let contextMenuClassName = "context-menu";
+    let contextMenuItemClassName = "context-menu__item";
+    let contextMenuLinkClassName = "context-menu__link";
+    let contextMenuActive = "context-menu--active";
+
+    let fileItemClassName = "file";
+    let fileItemInContext;
+
+    let clickCoords;
+    let clickCoordsX;
+    let clickCoordsY;
+
+    let menu = document.querySelector("#context-menu");
+    let menuItems = menu.querySelectorAll(".context-menu__item");
+    let menuState = 0;
+    let menuWidth;
+    let menuHeight;
+    let menuPosition;
+    let menuPositionX;
+    let menuPositionY;
+
+    let windowWidth;
+    let windowHeight;
+
+    /**
+     * Initialise our application's code.
+     */
+    function init() {
+        contextListener();
+        clickListener();
+        resizeListener();
+    }
+
+    /**
+     * Listens for contextmenu events.
+     */
+    function contextListener() {
+        document.addEventListener("contextmenu", function (e) {
+            fileItemInContext = clickInsideElement(e, fileItemClassName);
+            if (fileItemInContext) {
+                e.preventDefault();
+                toggleMenuOn();
+                positionMenu(e);
+            } else {
+                fileItemInContext = null;
+                toggleMenuOff();
+            }
+        });
+    }
+
+    /**
+     * Listens for click events.
+     */
+    function clickListener() {
+        let deleteBtn = document.getElementById("delete");
+        deleteBtn.addEventListener("click", menuActions.deleteElement, false);
+
+        let addBtn = document.getElementById("add");
+        addBtn.addEventListener("click", menuActions.addElement, false);
+
+        let renameBtn = document.getElementById("rename");
+        renameBtn.addEventListener("click", menuActions.renameElement, false);
+
+    }
+
+    /**
+     * Window resize event listener
+     */
+    function resizeListener() {
+        window.onresize = function (e) {
+            toggleMenuOff();
+        };
+    }
+
+    /**
+     * Turns the custom context menu on.
+     */
+    function toggleMenuOn() {
+        if (menuState !== 1) {
+            menuState = 1;
+            menu.classList.add(contextMenuActive);
+        }
+    }
+
+    /**
+     * Turns the custom context menu off.
+     */
+    function toggleMenuOff() {
+        if (menuState !== 0) {
+            menuState = 0;
+            menu.classList.remove(contextMenuActive);
+        }
+    }
+
+    function positionMenu(e) {
+        clickCoords = getPosition(e);
+        clickCoordsX = clickCoords.x;
+        clickCoordsY = clickCoords.y;
+
+        menuWidth = menu.offsetWidth + 4;
+        menuHeight = menu.offsetHeight + 4;
+
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+
+        let maxCoords = (e.clientX + 400);
+
+        if (clickCoordsX < maxCoords) {
+            menu.style.left = e.clientX + window.pageXOffset + "px";
+        } else {
+            menu.style.left = e.clientX - window.pageXOffset + "px";
+        }
+
+        if (clickCoordsY) {
+            menu.style.top = e.clientY + window.pageYOffset + "px";
+        } else {
+            menu.style.top = e.clientX - window.pageXOffset + "px";
+        }
+    }
+
+    function menuItemListener(link) {
+        console.log("file ID - " + fileItemInContext.getAttribute("id") + ", file action - " + link.getAttribute("data-action"));
+        toggleMenuOff();
+    };
+
+    menuActions.deleteElement = () => {
+        let element = fileItemInContext.getAttribute("id");
+        App.deleteFile(element);
+        toggleMenuOff();
+    };
+
+    menuActions.addElement = () => {
+        App.addFile();
+        toggleMenuOff();
+    };
+
+    menuActions.renameElement = () => {
+        let element = fileItemInContext.getAttribute("id");
+        App.renameFile(element);
+        toggleMenuOff();
+    };
+
+
+    /**
+     * Run the app.
+     */
+    init();
+})(window.App);
